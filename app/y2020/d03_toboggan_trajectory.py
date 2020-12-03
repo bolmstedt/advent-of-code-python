a68d6d7f3490a521661f0f61b9fbfcb4c221f05d
Marcus Bolmstedt <marcus@bolmstedt.com>
2020-12-03 13:09:12 +0100

    Add 2020:03

---
+++ app/y2020/d03_toboggan_trajectory.py
@@ -0,0 +1,46 @@
+"""Solution for day 3, 2020."""
+import math
+from typing import List, Union
+
+from app.base_solver import BaseSolver
+
+TREE = '#'
+
+
+class Solver(BaseSolver):
+    """Solver for day 3, 2020."""
+
+    day = '03'
+    year = '2020'
+    name = r"""Toboggan Trajectory"""
+
+    def part_one(self, data: str) -> Union[int, str]:
+        """Solve part one."""
+        rows = data.splitlines()
+
+        return self._get_hit_trees(rows, 3, 1)
+
+    def part_two(self, data: str) -> Union[int, str]:
+        """Solve part two."""
+        rows = data.splitlines()
+        speeds = [
+            (1, 1),
+            (3, 1),
+            (5, 1),
+            (7, 1),
+            (1, 2),
+        ]
+
+        return math.prod([
+            self._get_hit_trees(rows, speed_x, speed_y)
+            for speed_x, speed_y in speeds
+        ])
+
+    @staticmethod
+    def _get_hit_trees(rows: List[str], speed_x: int, speed_y: int) -> int:
+        map_width = len(rows[0])
+
+        return sum(
+            row[speed_x * (y + 1) % map_width] == TREE
+            for y, row in enumerate(rows[0::speed_y][1:])
+        )
