d2124e6128fe4b6406740caec6463a224fce4bd9
Marcus Bolmstedt <marcus@bolmstedt.com>
2020-12-03 13:54:57 +0100

    Refactor 2015:03

--- app/y2015/d03_perfectly_spherical_houses_in_a_vacuum.py
+++ app/y2015/d03_perfectly_spherical_houses_in_a_vacuum.py
@@ -1,7 +1,33 @@
 """Solution for day 3, 2015."""
-from typing import Union
+from typing import List, Union
 
 from app.base_solver import BaseSolver
+
+
+class _Sled:
+    """Keeps track of a sled's position."""
+
+    def __init__(self) -> None:
+        self._x = 0
+        self._y = 0
+
+    @property
+    def coords(self) -> str:
+        """Get Santa's position."""
+        return f'{self._x},{self._y}'
+
+    def move(self, direction: str) -> str:
+        """Move in one direction."""
+        if direction == '^':
+            self._y += 1
+        elif direction == 'v':
+            self._y -= 1
+        elif direction == '<':
+            self._x -= 1
+        elif direction == '>':
+            self._x += 1
+
+        return self.coords
 
 
 class Solver(BaseSolver):
@@ -13,50 +39,19 @@ class Solver(BaseSolver):
 
     def part_one(self, data: str) -> Union[int, str]:
         """Solve part one."""
-        santa = _Position()
-        grid = {santa.coords}
-
-        for direction in data:
-            grid.add(santa.move(direction))
-
-        return len(grid)
+        return self._visit_houses(data, [_Sled()])
 
     def part_two(self, data: str) -> Union[int, str]:
         """Solve part two."""
-        santa = _Position()
-        robo_santa = _Position()
-        grid = {santa.coords}
+        return self._visit_houses(data, [_Sled(), _Sled()])
 
-        for index, direction in enumerate(data):
-            if index % 2 == 0:
-                grid.add(santa.move(direction))
-            else:
-                grid.add(robo_santa.move(direction))
+    @staticmethod
+    def _visit_houses(data: str, sleds: List[_Sled]) -> int:
+        grid = {sleds[0].coords}
+        number_of_sleds = len(sleds)
+
+        for index, sled in enumerate(sleds):
+            for direction in data[index::number_of_sleds]:
+                grid.add(sled.move(direction))
 
         return len(grid)
-
-
-class _Position:
-    """Keeps track of Santa's position."""
-
-    def __init__(self) -> None:
-        self.x = 0
-        self.y = 0
-
-    @property
-    def coords(self) -> str:
-        """Get Santa's position."""
-        return f'{self.x},{self.y}'
-
-    def move(self, direction: str) -> str:
-        """Move in one direction."""
-        if direction == '^':
-            self.y += 1
-        elif direction == 'v':
-            self.y -= 1
-        elif direction == '<':
-            self.x -= 1
-        elif direction == '>':
-            self.x += 1
-
-        return self.coords
