0933c85053e9fe43539c206a7076bd1cca9b0906
Marcus Bolmstedt <marcus@bolmstedt.com>
2020-12-08 00:42:04 +0100

    Refactor 2020:07

--- app/y2020/d07_handy_haversacks.py
+++ app/y2020/d07_handy_haversacks.py
@@ -1,11 +1,55 @@
 """Solution for day 7, 2020."""
 import re
-from typing import Dict, List, Set, Tuple, Union
+from typing import Dict, List, Tuple, Union
 
 from app.base_solver import BaseSolver
 
-BAGS = re.compile(r'(.*) bags contain (.*)')
+BAGS = re.compile(r'^(.*) bags contain (.*)$', flags=re.MULTILINE)
 CONTENTS = re.compile(r'.*?(\d+) (.*?) bags?')
+TARGET_COLOR = 'shiny gold'
+
+
+class _RuleSet:
+    """Helper class for a set of rules."""
+
+    def __init__(self, data: str):
+        self._bags = self._parse_bags(data)
+        self._plain_bags = {
+            color: {c[1] for c in contents}
+            for color, contents in self._bags.items()
+        }
+
+    def get_bags_that_can_contain(
+        self,
+        color: str,
+    ) -> int:
+        new_bags = {
+            bag for bag, contents in self._plain_bags.items()
+            if color in contents
+        }
+
+        for bag in new_bags:
+            del self._plain_bags[bag]
+
+        return len(new_bags) + sum(
+            self.get_bags_that_can_contain(c) for c in new_bags
+        )
+
+    def bags_in_bag(
+        self,
+        color: str,
+    ) -> int:
+        return sum(
+            int(count) * (self.bags_in_bag(_color) + 1)
+            for count, _color in self._bags[color]
+        )
+
+    @staticmethod
+    def _parse_bags(data: str) -> Dict[str, List[Tuple[str, str]]]:
+        return {
+            bags.group(1): CONTENTS.findall(bags.group(2))
+            for bags in BAGS.finditer(data)
+        }
 
 
 class Solver(BaseSolver):
@@ -17,60 +61,8 @@ class Solver(BaseSolver):
 
     def part_one(self, data: str) -> Union[int, str]:
         """Solve part one."""
-        rules = {
-            color: {c[1] for c in contents}
-            for color, contents in self._parse_rules(data).items()
-        }
-        return len(self._get_bags_that_can_contain(
-            rules,
-            'shiny gold',
-        ))
+        return _RuleSet(data).get_bags_that_can_contain(TARGET_COLOR)
 
     def part_two(self, data: str) -> Union[int, str]:
         """Solve part two."""
-        return self._bags_in_bag(self._parse_rules(data), 'shiny gold')
-
-    @classmethod
-    def _get_bags_that_can_contain(
-        cls,
-        rules: Dict[str, Set[str]],
-        color: str,
-    ) -> Set[str]:
-        new_bags = {
-            bag for bag, contents in rules.items()
-            if color in contents
-        }
-
-        for bag in new_bags:
-            del rules[bag]
-
-        return new_bags.union(*[
-            cls._get_bags_that_can_contain(rules, c) for c in new_bags
-        ])
-
-    @classmethod
-    def _bags_in_bag(
-        cls,
-        bags: Dict[str, List[Tuple[str, str]]],
-        color: str,
-    ) -> int:
-        return sum(
-            int(count) + int(count) * cls._bags_in_bag(bags, c)
-            for count, c in bags[color]
-        )
-
-    @staticmethod
-    def _parse_rules(data: str) -> Dict[str, List[Tuple[str, str]]]:
-        rules = {}
-
-        for rule in data.splitlines():
-            bag_matches = BAGS.search(rule)
-
-            if not bag_matches:
-                continue
-
-            groups = bag_matches.groups()
-
-            rules[groups[0]] = CONTENTS.findall(groups[1])
-
-        return rules
+        return _RuleSet(data).bags_in_bag(TARGET_COLOR)
